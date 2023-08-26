69ef6b7c71cc65750ed8757d3fbb4e371b4f2ff3
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-08-26 21:20:43 +0700

    Add text support

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use logger;
 use minifb::{Window, WindowOptions};
+use rusttype::{Font, Scale, point, PositionedGlyph};
 
 pub fn say_hi() {
     logger::log(
@@ -40,9 +41,10 @@ pub struct Screen {
     pub buffer: Vec<u32>,
     pub max_update_time_as_micros: u128,
     pub max_update_time_as_millis: f64,
+    pub font_data: Vec<u8>,
 }
 impl Screen {
-    pub fn new(height: usize, width: usize, scale: usize, title: &'static str, fps: usize, window_options: WindowOptionsSettings) -> Screen {
+    pub fn new(height: usize, width: usize, scale: usize, title: &'static str, fps: usize, window_options: WindowOptionsSettings, font: Vec<u8>) -> Screen {
         return Screen {
             size: (height, width),
             scale: scale,
@@ -67,7 +69,8 @@ impl Screen {
             }),
             buffer: vec![0xFF_000000; width * height],
             max_update_time_as_micros: 1000000 / fps as u128,
-            max_update_time_as_millis: (1000000.0 / fps as f64) / 1000.0 ,
+            max_update_time_as_millis: (1000000.0 / fps as f64) / 1000.0,
+            font_data: font.clone(),
         }
     }
     pub fn is_open(&self) -> bool {
@@ -140,5 +143,31 @@ impl Screen {
                 self.buffer[buffer_index] = sprite[sprite_index];
             }
         };
+    }
+    pub fn draw_text(&mut self, pos_y: usize, pos_x: usize, size: usize, color: u32, secondary_color: u32, text: &str) {
+        let font = Font::try_from_bytes(self.font_data.as_slice()).expect("Error loading font");
+        let scale = Scale::uniform(size as f32);
+        let v_metrics = font.v_metrics(scale);
+        let glyphs: Vec<PositionedGlyph> = font.layout(text, scale, point(pos_x as f32, pos_y as f32 + v_metrics.ascent)).collect();
+
+        for glyph in glyphs {
+            if let Some(bounding_box) = glyph.pixel_bounding_box() {
+                glyph.draw(|x, y, v| {
+                    let x = x + bounding_box.min.x as u32 + pos_x as u32;
+                    let y = y + bounding_box.min.y as u32 + pos_y as u32;
+                    let index = y * self.size.1 as u32 + x;
+                    if v > 0.65 {
+                        self.buffer[index as usize] = color;
+                    } else if v > 0.45 {
+                        self.buffer[index as usize] = secondary_color;
+                    }
+                });
+            } else {
+                logger::log(
+                    logger::PREFIX_WARN,
+                    format!("Character {} not found in font", text).as_str()
+                );
+            }
+        }
     }
 }
